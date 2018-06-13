0f617e5489d363f05c343f5697606f542def2ff1
Alex Xin <xinba1989918@gmail.com>
2018-06-13 20:27:23 +1000

    change microbreak ideas

--- app/utils/defaultMicrobreakIdeas.js
+++ app/utils/defaultMicrobreakIdeas.js
@@ -1,6 +1,7 @@
 module.exports = [
-  { data: 'Go grab a glass of water.', enabled: true },
-  { data: 'Slowly look all the way left, then right.', enabled: true },
+  { data: '去喝水啦～～～～', enabled: true },
+  { data: 'if 站着， then 坐下 else 站起来', enabled: true }
+/*  { data: 'Slowly look all the way left, then right.', enabled: true },
   { data: 'Slowly look all the way up, then down.', enabled: true },
   { data: 'Close your eyes and take few deep breaths.', enabled: true },
   { data: 'Close your eyes and relax.', enabled: true },
@@ -11,5 +12,5 @@ module.exports = [
   { data: 'Slowly tilt head to side and hold for 5-10 seconds.', enabled: true },
   { data: 'Stand from chair and stretch.', enabled: true },
   { data: 'Refocus eyes on an object at least 20 meters away.', enabled: true },
-  { data: 'Take a moment to think about something you appreciate.', enabled: true }
+  { data: 'Take a moment to think about something you appreciate.', enabled: true }*/
 ]
